b5a0da863f5a2b8c8e6b9374a4d7b4e4ee8baeae
Mike Doran <mikeybyker@gmail.com>
2016-07-14 19:53:39 +0100

    Clean up

--- www/js/ionicfm/sw.ionicfm.js
+++ www/js/ionicfm/sw.ionicfm.js
@@ -2,7 +2,6 @@
     'use strict';
     
     angular
-        .module('sw.ionicfm', [
-            
-        ]);
+        .module('sw.ionicfm', []);
+        
 }());
--- www/js/ionicfm/utilities/utilities.service.js
+++ www/js/ionicfm/utilities/utilities.service.js
@@ -5,46 +5,47 @@
         .module('sw.ionicfm')
         .factory('Utilities', Utilities);
 
-        function Utilities($log, ModalService) {
+    function Utilities($log, ModalService) {
 
-            var placeholder = 'http://placehold.it/174x174';
+        var placeholder = 'http://placehold.it/174x174';
 
-            var Util = {
+        var Util = {
 
-                getImage : function(collection, size){
-                    var imgsrc = '';
-                    if(collection.image && collection.image.length){
-                        imgsrc = _.result(_.findWhere(collection.image, { 'size': size }), '#text');
-                    }
-                    if(collection && !imgsrc){
-                        imgsrc = placeholder + '?text=no+image+for+' + collection.name;
-                    }
-                    return imgsrc;
-                },
-                getDuration : function(duration){
-                    var mins = ~~(duration / 60),
-                        secs = duration % 60,
-                        pretty = '' + mins + ':' + (secs < 10 ? '0' : '');
-                    pretty += '' + secs;
-                    return pretty;
-                },
-                openLink : function(url){
-                    window.open(url, '_blank');
-                },
-                showDataError :function(reason){
-                    return this.showAlert({
-                                            body:reason.statusText || 'Bit of a problem loading data...Sorry.',
-                                            title: reason.title || 'Server Error'
-                                        });
-                },
-                showAlert :function(message){
-                    var params = angular.extend(
-                                {title:'Error', body:'Sorry, there has been an error.'},
-                                message || {});
-                    return ModalService.show('error-modal.html', 'AlertController as vm', params);
+            getImage : function(collection, size){
+                var imgsrc = '';
+                if(collection.image && collection.image.length){
+                    imgsrc = _.result(_.findWhere(collection.image, { 'size': size }), '#text');
                 }
+                if(collection && !imgsrc){
+                    imgsrc = placeholder + '?text=no+image+for+' + collection.name;
+                }
+                return imgsrc;
+            },
+            getDuration : function(duration){
+                var mins = ~~(duration / 60),
+                    secs = duration % 60,
+                    pretty = '' + mins + ':' + (secs < 10 ? '0' : '');
+                pretty += '' + secs;
+                return pretty;
+            },
+            openLink : function(url){
+                window.open(url, '_blank');
+            },
+            showDataError :function(reason){
+                return this.showAlert({
+                                        body:reason.statusText || 'Bit of a problem loading data...Sorry.',
+                                        title: reason.title || 'Server Error'
+                                    });
+            },
+            showAlert :function(message){
+                var params = angular.extend(
+                            {title:'Error', body:'Sorry, there has been an error.'},
+                            message || {});
+                return ModalService.show('error-modal.html', 'AlertController as vm', params);
             }
-
-            return Util;
         }
+
+        return Util;
+    }
+    
 }());
